d78ec192eae25e22205ee9d9c35aea1f565b4694
Nicholas Schenone <Nick@Nicks-MacBook-Pro-2.local>
2018-10-23 15:02:56 -0700

    Finished documentation for Slot

--- Slot.py
+++ Slot.py
@@ -67,17 +67,50 @@ class Slot():
            
     # Get random note from scale and random rhythm value
     def rand(self, scale, custom_len_list=None, custom_len_mod_list=None):
+        """Class method to randomize note and rhythm values of Slot. 
+        
+        :param scale: Scale object to pick random note from 
+        :param custom_len_list: Optional list of custom length values 
+        :param custom_len_mod_list: Optional list of custom length modifier values 
+        
+        :type scale: Scale 
+        :type custom_len_list: List of Strings 
+        :type custom_len_mod_list: List of Strings 
+
+        :return: No return, modifys existing object 
+        :rtype: None 
+        """
         self.rand_note(scale)
         self.rand_rhythm(custom_len_list, custom_len_mod_list)
         return self
 
     # Get random note from scale
     def rand_note(self, scale):
+        """Class method to get random note from scale. 
+        
+        :param scale: Scale object to pick random note from 
+        
+        :type scale: Scale 
+
+        :return: No return, modifys existing object 
+        :rtype: None 
+        """
         self.note = random.choice(scale.notes)
         return self
 
     # Get random rhythm value
     def rand_rhythm(self, custom_len_list=None, custom_len_mod_list=None):
+        """Class method to randomize rhythm value of Slot. 
+        
+        :param custom_len_list: Optional list of custom length values 
+        :param custom_len_mod_list: Optional list of custom length modifier values 
+        
+        :type custom_len_list: List of Strings 
+        :type custom_len_mod_list: List of Strings 
+
+        :return: No return, modifys existing object 
+        :rtype: None 
+        """
         # Get random rhythm from class dict or custom dict
         if custom_len_list is None:
             self.length = random.choice(self.SLOT_LEN_DICT)
@@ -93,13 +126,43 @@ class Slot():
         return self
 
     # Mutate note and rhythm value
-    def mutate(self, scale, prob=1):
+    def mutate(self, scale, prob=1, custom_len_list=None, custom_len_mod_list=None):
+        """Class method to mutate note and rhythm values of Slot. 
+        
+        :param scale: Scale object to pick random note from
+        :param prob: Probability that Slot is mutated
+        :param custom_len_list: Optional list of custom length values 
+        :param custom_len_mod_list: Optional list of custom length modifier values 
+        
+        :type scale: scale
+        :type prob: int
+        :type custom_len_list: List of Strings 
+        :type custom_len_mod_list: List of Strings 
+
+        :return: No return, modifys existing object 
+        :rtype: None 
+        """
         self.mutate_note(scale, prob)
         self.mutate_rhythm()
         return self
 
     # Mutate note based on scale (returns adjacent note in scale)
     def mutate_note(self, scale, prob=1, threshold=0.5):
+        """Class method to mutate note value of Slot. 
+        
+        :param scale: Scale object to pick random note from
+        :param prob: Probability that Slot is mutated
+        :param threshold: Probability of mutating note up or down
+
+        :type scale: scale
+        :type prob: int
+        :type threshold: int
+
+        :return: No return, modifys existing object 
+        :rtype: None 
+        """
+        self.mutate_note(scale, prob)
+        self.mutate_rhythm()
         # Don't mutate if random prob is larger than prob
         if random.random() > prob:
             return self
@@ -136,21 +199,67 @@ class Slot():
 
     # Mutate rhythm value
     def mutate_rhythm(self):
+        """WORK IN PROGRESS - Class method to mutate rhythm value of Slot. 
+        
+        :param prob: Probability that Slot is mutated
+        :param custom_len_list: Optional list of custom length values 
+        :param custom_len_mod_list: Optional list of custom length modifier values 
+        
+        :type prob: int
+        :type custom_len_list: List of Strings 
+        :type custom_len_mod_list: List of Strings 
+
+        :return: No return, modifys existing object 
+        :rtype: None 
+        """
         return self
 
     # Set note and rhythm value
     def set(self, note, length, length_mod):
+        """Class method to set note and rhythm value of Slot. 
+        
+        :param note: Note to set Slot to 
+        :param length: Length to set Slot to 
+        :param length_mod: Length mod to set Slot to 
+        
+        :type note: Note 
+        :type length: String 
+        :type length_mod: String 
+
+        :return: No return, modifys existing object 
+        :rtype: None 
+        """
         self.set_note(note)
         self.set_rhythm(length, length_mod)
         return self
 
     # Set note
     def set_note(self, note):
+        """Class method to set note value of Slot. 
+        
+        :param note: Note to set Slot to 
+        
+        :type note: Note 
+
+        :return: No return, modifys existing object 
+        :rtype: None 
+        """
         self.note = note
         return self
 
     # Set rhythm value
     def set_rhythm(self, length, length_mod):
+        """Class method to set rhythm value of Slot. 
+        
+        :param length: Length to set Slot to 
+        :param length_mod: Length mod to set Slot to 
+        
+        :type length: String 
+        :type length_mod: String 
+
+        :return: No return, modifys existing object 
+        :rtype: None 
+        """
         self.length = length
         self.length_mod = self.SLOT_LEN_MOD_DICT.get(length_mod)
         return self
