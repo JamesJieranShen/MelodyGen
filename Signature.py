ad227a103fffc856b3adfc1654e4098d5399c40c
James Shen <jamesjieranshen@gmail.com>
2019-04-21 02:24:45 -0700

    Init Implement Signature.py

--- Signature.py
+++ Signature.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr//env python3
 """
 .. module:: Signature
    :platform: Mac, Unix, Windows
@@ -37,7 +37,20 @@ class Signature():
             self.num_beats = num_beats
             self.beat = Fraction(beat) # Deep Copy
             self._accents = [False] * num_beats
-        
+
+    def isValid(self):
+        """Validity checker for Signature.
+        :param:     NONE
+
+        :return:    True if signature is valid.
+        :rtype:     Boolean
+        """
+        if self.num_beats != len(self._accents): 
+            return False
+        return True;
+
+# Methods related to accents
+
     def get_accents(self, location = 1):
         """ Getter for _accents.
         :param location:    Location of the beat in the measure.
@@ -96,17 +109,47 @@ class Signature():
         """
         
         if location > self.num_beats or location < 1:
-            print("CATASTROPHIC FAILURE: {0} is not a valid location".
-                    format(location))
+            print("CATASTROPHIC FAILURE: {0} is not a valid location.".
+             format(location))
             return False
         self._accents[location] = False
         return self._accents[location]
+    def set_accent(self, accent_list):
+        """Set accent according to accent_list
+        :param accent_list:    List specifying accents. True is accent.
+        :type accent_list:     list of boolean.
 
-    def isValid(self):
-        # TODO
-        pass
+        :return:                True for success
+        :rtype:                 boolean
+        """
+        if len(accent_list) != self.num_beats:
+            print("CATASTROPHIC FAILURE: list is not valid.")
+            return False
+        for i in range(accent_list):
+            self._accents[] = accent_list[i]
+        return True
 
-    def __str____(self):
+# Other Setters and Getters
+    def get_beat(self):
+        return Fraction(self.beat)
+
+    def get_num_beats(self):
+        return self.num_beats
+
+    def set_beat(self, beat):
+        self.beat = Fraction(beat)
+
+    def set_num_beats(self, num_beats):
+        self.num_beats = num_beats
+    
+    def set_signature(self, num_beats, beatValue):
+        self.num_beats = num_beats
+        if isinstance(beatValue, int):
+            self.beat = Fraction(1/beatValue)
+        else:
+            self.beat = Fraction(beatValue)
+
+    def __str__(self):
         return """<Signature: %s  accents: [%s]>""" 
                 % ( Fraction(self.num_beats * self.beat)
                     " ".join(str(x) for x in self._accents
