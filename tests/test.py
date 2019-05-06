338b837f25f38cb3866e3d3f3a7817236370d0b7
Nicholas Schenone <Nick@Nicks-MacBook-Pro-5.local>
2019-05-06 11:24:41 -0700

    Updated formatting

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 from context import melodygen as gen
-from melodygen import Note, Scale, Phrase, MIDIHandler as handler, Generate, constants as const
+from melodygen import (
+    Note,
+    Scale,
+    Phrase,
+    MIDIHandler as handler,
+    Generate,
+    constants as const,
+)
 import mido
 import time
 import random
 
-# Scales 
-e = gen.Scale("E", 'MAJOR', 3, 3)
-g = gen.Scale("G", 'MAJOR', 3, 3)
-c = gen.Scale("C", 'MAJOR_PENT', 4, 2)
+# Scales
+e = gen.Scale("E", "MAJOR", 3, 3)
+g = gen.Scale("G", "MAJOR", 3, 3)
+c = gen.Scale("C", "MAJOR_PENT", 4, 2)
 d = gen.Scale("D", "HARM_MINOR", 4, 2)
 
 scales = [e, c, g, d]
@@ -33,25 +40,25 @@ for i in range(phrase_len):
     phrase.append(gen.Note(length=1/16,
         length_mod=random.choice([1, 1.5, 2/3]), scale=random.choice(scales), prob=0.8))
 """
-phrase.generate_phrase("MapMod", {"input": "../melodygen/gen/pi.txt", "scales":
-    scales,
-    "gen_len": 8})
+phrase.generate_phrase(
+    "MapMod", {"input": "../melodygen/gen/pi.txt", "scales": scales, "gen_len": 8}
+)
 
-while(True):
+while True:
     phrase.play()
-    #phrase.unify_prob()
+    # phrase.unify_prob()
     if random.random() < 0.2:
         if random.random() < 0.5:
             phrase.reverse()
             print("Reversed")
         else:
-            phrase.flip();
+            phrase.flip()
             print("Flipped")
     for note in phrase.phrase:
-        note.set_length(1/16)
+        note.set_length(1 / 16)
         note.rand_note(scale=random.choice(scales), prob=0.15)
         note.mutate_length(prob=0.05)
-#print(e)
+# print(e)
 """
 
 while(True):
--- tests/test2.py
+++ tests/test2.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 from context import melodygen as gen
-from melodygen import Note, Scale, Phrase, MIDIHandler as handler, Generate, constants as const
+from melodygen import (
+    Note,
+    Scale,
+    Phrase,
+    MIDIHandler as handler,
+    Generate,
+    constants as const,
+)
 import mido
 import time
 import random
 
-# Scales 
-e = gen.Scale("E", 'MAJOR', 2, 3)
-g = gen.Scale("G", 'MAJOR', 2, 3)
-c = gen.Scale("C", 'MAJOR_PENT', 3, 2)
+# Scales
+e = gen.Scale("E", "MAJOR", 2, 3)
+g = gen.Scale("G", "MAJOR", 2, 3)
+c = gen.Scale("C", "MAJOR_PENT", 3, 2)
 d = gen.Scale("D", "HARM_MINOR", 3, 2)
 
 scales = [e, c, g, d]
@@ -20,11 +27,18 @@ play_len = 5
 # Phrase
 phrase = gen.Phrase(tempo=120, debug=True, endless=True)
 
-phrase.generate_phrase("MapMod", {"input": "../melodygen/gen/pi.txt", "scales":
-    scales, "gen_len": 2, "start_offset": 0})
+phrase.generate_phrase(
+    "MapMod",
+    {
+        "input": "../melodygen/gen/pi.txt",
+        "scales": scales,
+        "gen_len": 2,
+        "start_offset": 0,
+    },
+)
 
-while(True):
+while True:
     for note in phrase.phrase:
-        note.set_length(1/8)
+        note.set_length(1 / 8)
 
     phrase.play()
