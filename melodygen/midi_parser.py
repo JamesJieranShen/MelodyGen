433e8680faade9300ad1134767876b8ab7dca5e6
Nicholas Schenone <Nick@Nicks-MacBook-Pro-5.local>
2019-05-08 13:42:48 -0700

    Working on parser - extracted pitches, length, offset

--- melodygen/midi_parser.py
+++ melodygen/midi_parser.py
@@ -3,30 +3,38 @@ import pickle
 import numpy
 from music21 import converter, instrument, note, chord
 
+
 def get_notes():
     """ Get all the notes and chords from the midi files in the ./midi_songs directory """
     notes = []
-
-    for file in glob.glob("midi_songs/*.mid"):
+    for file in glob.glob("midi_songs/cosmo.mid"):
         midi = converter.parse(file)
 
         print("Parsing %s" % file)
 
         notes_to_parse = None
 
-        try: # file has instrument parts
+        try:  # file has instrument parts
             s2 = instrument.partitionByInstrument(midi)
-            notes_to_parse = s2.parts[0].recurse() 
-        except: # file has notes in a flat structure
+            notes_to_parse = s2.parts[0].recurse()
+        except:  # file has notes in a flat structure
             notes_to_parse = midi.flat.notes
 
         for element in notes_to_parse:
             if isinstance(element, note.Note):
-                notes.append(str(element.pitch))
+                notes.append(element)
+                # notes.append(str(element.pitch))
             elif isinstance(element, chord.Chord):
-                notes.append('.'.join(str(n) for n in element.normalOrder))
+                pass
+                # notes.append(".".join(str(n) for n in element.normalOrder))
 
     # with open('data/notes', 'wb') as filepath:
     #     pickle.dump(notes, filepath)
 
-    print notes
+    for asdf in notes[:5]:
+        print("pitch", asdf.pitch.midi)
+        print("offset", asdf.offset)
+        print("length", asdf.quarterLength)
+
+
+get_notes()
